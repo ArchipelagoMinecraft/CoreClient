332293219a76d033310f6693120614ac19a929dc
Elly <eric.poore@gmail.com>
2026-01-21 21:02:54 -0600

    APClient Setup + Command Registry

---
+++ src/main/java/io/github/archipelagominecraft/core/APClient/APClient.java
@@ -0,0 +1,46 @@
+package io.github.archipelagominecraft.core.APClient;
+
+import io.github.archipelagominecraft.core.ArchipelagoMinecraftClientCore;
+import io.github.archipelagomw.Client;
+import io.github.archipelagomw.flags.ItemsHandling;
+
+import java.net.URISyntaxException;
+
+public class APClient extends Client {
+    private final APContext ctx;
+
+    public APClient(APContext ctx) {
+        super();
+        this.ctx = ctx;
+        // Change this name to pull from a config file or some equivalent later
+        this.setGame("Minecraft");
+    }
+
+    public static int tryConnection(APContext context, String address, int port, String slot, String password) {
+        APClient apClient = context.getClient();
+
+        apClient.setPassword(password);
+        apClient.setName(slot);
+        apClient.setItemsHandlingFlags(ItemsHandling.SEND_ITEMS + ItemsHandling.SEND_OWN_ITEMS + ItemsHandling.SEND_STARTING_INVENTORY);
+
+        try {
+            String server = address + ":" + port;
+            apClient.connect(server);
+        } catch (URISyntaxException e) {
+            //Replace with a useful chat error instead later
+            ArchipelagoMinecraftClientCore.LOGGER.error(e.toString());
+        }
+        return 0;
+    }
+
+
+    @Override
+    public void onError(Exception ex) {
+
+    }
+
+    @Override
+    public void onClose(String Reason, int attemptingReconnect) {
+
+    }
+}
---
+++ src/main/java/io/github/archipelagominecraft/core/events/commands/APConnectCmd.java
@@ -0,0 +1,35 @@
+package io.github.archipelagominecraft.core.events.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import io.github.archipelagominecraft.core.APClient.APClient;
+import io.github.archipelagominecraft.core.APClient.APContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+
+public class APConnectCmd {
+    public static void connectCommand(CommandDispatcher<CommandSourceStack> command) {
+        // Connection Command
+        command.register(Commands.literal("connect")
+            .then(Commands.argument("server", StringArgumentType.string()))
+            .then(Commands.argument("port", IntegerArgumentType.integer()))
+            .then(Commands.argument("slot", StringArgumentType.string()))
+                .executes(context -> APClient.tryConnection(
+                        APContext.getContext(),
+                        StringArgumentType.getString(context, "server"),
+                        IntegerArgumentType.getInteger(context, "port"),
+                        StringArgumentType.getString(context, "slot"),
+                        ""
+                ))
+                .then(Commands.argument("password", StringArgumentType.string()))
+                    .executes(context -> APClient.tryConnection(
+                            APContext.getContext(),
+                            StringArgumentType.getString(context, "server"),
+                            IntegerArgumentType.getInteger(context, "port"),
+                            StringArgumentType.getString(context, "slot"),
+                            StringArgumentType.getString(context, "password")
+                    ))
+        );
+    }
+}
