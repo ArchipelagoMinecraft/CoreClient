ab915fd8aa1d5d5ca39cf58bee4e82a2b5a46e55
Emily <eric.poore@gmail.com>
2026-02-01 03:39:11 -0600

    Add neoforge support

--- src/main/java/io/github/archipelagominecraft/core/ArchipelagoMinecraftClientCore.java
+++ src/main/java/io/github/archipelagominecraft/core/ArchipelagoMinecraftClientCore.java
@@ -3,7 +3,13 @@ package io.github.archipelagominecraft.core;
 
 import com.mojang.logging.LogUtils;
 import io.github.archipelagominecraft.core.events.commands.APConnectCmd;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+//? if fabric {
+/*import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+*///?} elif neoforge {
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+//?}
 import org.slf4j.Logger;
 
 public class ArchipelagoMinecraftClientCore {
@@ -12,9 +18,19 @@ public class ArchipelagoMinecraftClientCore {
     public static void initialize() {
         LOGGER.info("Hello from ArchipelagoMinecraftClientCore!");
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+        //? if fabric {
+        /*CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             APConnectCmd.connectCommand(dispatcher);
         });
+        *///?}
+
     }
+
+    //? if neoforge {
+    @SubscribeEvent
+    static void onRegisterCommandsEvent(RegisterCommandsEvent event) {
+        APConnectCmd.connectCommand(event.getDispatcher());
+    }
+    //?}
 }
 //?}
